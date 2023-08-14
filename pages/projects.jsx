dd0822231d7c54e89a05e132d416441e7ae48d07
Achraf333-m <achrafdaimallah2003@gmail.com>
2023-08-13 23:54:32 -0400

    project update

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -12,9 +12,11 @@ import libraryCart from "@/public/library-cart.png";
 import loading from "@/public/Skeleton-loading.png";
 import marketplace from "@/public/internship-landing.png";
 import marketplaceExplore from "@/public/intern-explore.png";
-import moviePalace from "@/public/Screenshot 2023-07-28 012453.png";
 import TicTacToe from "@/public/TicTacToe.png";
 import portfolio from "@/public/portfolio.png";
+import chatApp from "@/public/chatApp.png";
+import login from "@/public/loginPage.png";
+import signUp from "@/public/SignUpPage.png";
 import portfolioMessage from "@/public/portfolio-message.png";
 import Image from "next/image";
 import AOS from "aos";
@@ -25,7 +27,7 @@ import { useEffect } from "react";
 
 function Projects() {
   useEffect(() => {
-    AOS.init({ duration:1500, once: true });
+    AOS.init({ duration: 1500, once: true });
   }, []);
   return (
     <>
@@ -33,18 +35,33 @@ function Projects() {
         <title>My projects</title>
       </Head>
       <div className="darkMode lightMode">
-      <Header />
+        <Header />
         <section className="  flex flex-col max-w-6xl mx-auto px-10 overflow-hidden">
-          <h1 className="mt-16 mb-20 text-3xl font-semibold" data-aos="fade-in">My Projects</h1>
+          <h1 className="mt-16 mb-20 text-3xl font-semibold" data-aos="fade-in">
+            My Projects
+          </h1>
           <div className="pb-44 flex flex-col space-y-4">
-            <h3 className="font-light text-4xl" data-aos="fade-in" data-aos-delay="300">some of my clones:</h3>
+            <h3
+              className="font-light text-4xl"
+              data-aos="fade-in"
+              data-aos-delay="300"
+            >
+              some of my clones:
+            </h3>
             <br />
             <p className="text-xl" data-aos="fade-in" data-aos-delay="500">
-              These projects were made using various APIs, CD/CI, libraries, frameworks and some backend as well.
+              These projects were made using various APIs, CD/CI, libraries,
+              frameworks and some backend as well.
             </p>
             <div data-aos="fade-in" className="space-y-10">
               <div className="">
-                <h1 className="text-3xl" data-aos="fade-in" data-aos-delay="300">Netflix</h1> 
+                <h1
+                  className="text-3xl"
+                  data-aos="fade-in"
+                  data-aos-delay="300"
+                >
+                  Netflix
+                </h1>
                 <p>NextJS, TypeScript, TailwindCSS, MUI, Firebase</p>
                 <Links
                   Wlink="https://netflix-cloned-one.vercel.app/"
@@ -99,9 +116,7 @@ function Projects() {
             </div>
           </div>
           <div className="pb-20 flex flex-col space-y-4">
-            <h1 className="font-light text-2xl">
-              More Projects
-            </h1>
+            <h1 className="font-light text-2xl">More Projects</h1>
             <div className="space-y-10">
               <div data-aos="fade-in">
                 <h1 className="text-3xl">Library app</h1>
@@ -135,7 +150,10 @@ function Projects() {
               </div>
               <div>
                 <h1 className="text-3xl">E-currency Marketplace</h1>
-                <h6 className="text-xs">A forked branch that I worked on during my internship using branches off main that were merged afterwards</h6>
+                <h6 className="text-xs">
+                  A forked branch that I worked on during my internship using
+                  branches off main that were merged afterwards
+                </h6>
                 <p>GIT, React, JavaScript, BootstrapCSS </p>
                 <Links
                   Wlink="https://achraf-internship.vercel.app/"
@@ -162,17 +180,18 @@ function Projects() {
             <div>
               <div className="space-y-10">
                 <div data-aos="fade-in">
-                  <h1 className="text-3xl">Movie Palace</h1>
+                  <h1 className="text-3xl">Awesome Chat</h1>
+                  <p>NextJS, SASS, Mixin, Firebase v9 </p>
                   <Links
-                    Wlink="https://achraf333-m.github.io/MovieDA/"
-                    Glink="https://github.com/Achraf333-m/MovieDA"
+                    Wlink="https://chat-app-ten-rouge.vercel.app/"
+                    Glink="https://github.com/Achraf333-m/Chat-app"
                   />
                   <div className="project-style">
                     <Image
                       alt="section-image"
-                      className="hover-transition"
+                      className="hover-transition lg:!w-[500px]"
                       data-aos="fade-right"
-                      src={moviePalace}
+                      src={chatApp}
                     />
                   </div>
                 </div>
@@ -218,12 +237,11 @@ function Projects() {
           </div>
           <div className="space-y-4 md:space-y-0 flex flex-col md:flex-row md:space-x-4 items-center justify-center py-10 mb-20">
             <p>
-              You have a project for me or want me to work with you? You can easily contact me here!
+              You have a project for me or want me to work with you? You can
+              easily contact me here!
             </p>
             <Link href="/contact">
-              <button className="btn">
-                Contact me
-              </button>
+              <button className="btn">Contact me</button>
             </Link>
           </div>
         </section>
